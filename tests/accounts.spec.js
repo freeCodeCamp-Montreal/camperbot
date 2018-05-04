f52952ad3063b3f4bfa622082de2b71333b50e5c
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2018-05-04 17:27:31 -0400

    :sparkles: Add simple tests

--- tests/accounts.spec.js
+++ tests/accounts.spec.js
@@ -1,22 +1,23 @@
 // Jest automatically has babel-jest installed :D
 import '../src/libs/dotenv';
 import { knex, pgp } from '../src/libs/postgres';
+import { insertAccount } from '../src/queries';
 
-let id;
+let discordId;
 
 beforeEach(async () => {
-  [id] = await knex('accounts')
+  [discordId] = await knex('accounts')
     .insert({
       discord_id: '143509445354323968',
       created_at: '2018-04-07',
     })
-    .returning('id');
+    .returning('discord_id');
 });
 
 afterEach(async () => {
   await knex('accounts')
     .delete()
-    .where({ id });
+    .where({ discord_id: discordId });
 });
 
 afterAll(async () => {
@@ -24,6 +25,22 @@ afterAll(async () => {
   await knex.destroy();
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+test('Successfully inserts an account', async () => {
+  insertAccount({
+    discordId: '153509445354323968',
+    callback: () => {
+      done();
+    },
+  });
+});
+
+test('Fail to inserts an account with a duplicate discordId', async () => {
+  insertAccount({
+    discordId,
+    callback: () => {},
+    error: err => {
+      expect(err.code).toBe('23505');
+      done();
+    },
+  });
 });
